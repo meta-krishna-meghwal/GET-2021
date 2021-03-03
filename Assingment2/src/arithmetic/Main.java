c46ecc1f4c1c59154023b31447d64bd56bbdcc59
krishna meghwal <krishna.meghwal@metacube.com>
2021-03-03 19:42:48 +0530

    Assingment : changes using inbuilt method

--- Assingment2/src/arithmetic/Main.java
+++ Assingment2/src/arithmetic/Main.java
@@ -1,9 +1,17 @@
 package arithmetic;
 
 public class Main {
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		HexCalc h = new HexCalc();
-		System.out.println(h.lessThan("abcd","bcd"));
+		System.out.println(h.hexAddition("bcd", "bcd"));
+		System.out.println(h.hexMultiplication("bcd", "bcd"));
+		System.out.println(h.hexSubstraction("bcd", "bcd"));
+		System.out.println(h.hexDivision("bcd", "bcd"));
+		System.out.println(h.hexToDec("bcd"));
+		System.out.println(h.decToHex(3021));
+		System.out.println(h.lessThan("bcd", "ccd"));
+		System.out.println(h.greaterThan("bcd", "ccd"));
+		System.out.println(h.equalsTo("bcd", "ccd"));
 	}
 }
